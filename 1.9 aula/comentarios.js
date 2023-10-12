3a8918983704105f8245024b096ec0b1a2667c3d
GlauReg33 <145076142+GlauReg33@users.noreply.github.com>
2023-10-12 06:17:19 -0300

    Initial commit

---
+++ 1.9 aula/comentarios.js
@@ -0,0 +1,24 @@
+/**
+ * arquivo: comentario.js
+ * descrição: explicação de como podemos usar comentários em JavaScript
+ * author: Glaucia Lemos 
+ * /
+ 
+
+// --> Single Line Comments //
+let animal = 'cachorro';
+
+
+
+// --> Multiple line comments /* */
+
+console.log (animal);
+
+let num_3 = 30;
+let num_4 = 50;
+let num_1 = 10;
+let num_2 = 20;
+
+let somartodos =  num_1 + num_2 + num_3 + num_4;
+console.log (somattodos)
+
---
+++ variaveis.js
@@ -0,0 +1,31 @@
+// Uso de variaveis em JavaScript
+
+// Soma = num1 + num2
+
+var num1 = 10;
+var num2 = 20;
+
+var total = num1 + num2;
+console.log (total);
+
+//let 
+let saudacao = " Olá, Programadoras!";
+
+if (treu) {
+    let saudacao = "Quero aprender JavaScript";
+    console.log(saudacao);
+}
+
+console.log(saudacao)
+
+//const
+
+const cidade = "Rio de Janeiro";
+
+function estado ( ) {
+    const cidade = "São Pualo";
+    console.log (cidade);
+}
+
+estado ()
+console.log (cidade);
